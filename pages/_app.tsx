5e375bba61df9dd75b47f13e4ccb916c269e9083
Dasun Sucharith <sucharith.dasun@gmail.com>
2025-06-05 10:58:44 +0530

    chore: restructure for next pages

---
+++ pages/_app.tsx
@@ -0,0 +1,24 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { ComponentType } from "react";
+
+const queryClient = new QueryClient();
+
+interface AppProps {
+  Component: ComponentType<Record<string, unknown>>;
+  pageProps: Record<string, unknown>;
+}
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <Component {...pageProps} />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
